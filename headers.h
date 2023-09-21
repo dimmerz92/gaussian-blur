d94874b1c27068141b2116f0e135d8d6c39d802f
Andrew <andrew.weymes@outlook.com>
2023-09-21 14:06:09 +0800

    add headers file

---
+++ headers.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpi.h"
+
+#include "qdbmp.h"
+#include "utilities.h"
+#include "gaussianLib.h"
+
+#define ROOT 0
+#define KERNEL_DIMENSION_SD 3
+#define KERNEL_BITMAP_FILENAME "kernel.bmp"
