e65209977f0783bddfc666fbe7d84171736c2cf3
Andrew <andrew.weymes@outlook.com>
2023-09-22 15:03:11 +0800

    refactor functions

--- utilities.c
+++ utilities.c
@@ -28,7 +28,7 @@ int is_positive_number(char *str) {
 }
 
 /* Parse args to check correct types and formats passed */
-int parse_args(int argc, char *argv[], float *std_dev) {
+int parse_args(int argc, char *argv[], double *std_dev) {
     if (argc != 4) {
         fprintf(stderr,
                 "Usage: %s <input_file> <output_file> <std_dev>",
@@ -49,7 +49,7 @@ int parse_args(int argc, char *argv[], float *std_dev) {
 }
 
 /* frees the kernel items */
-void free_kernels(float **kernel, float kernel_dim) {
+void free_kernels(double **kernel, double kernel_dim) {
     for (int i = 0; i < kernel_dim; i++) {
         free(kernel[i]);
     }
@@ -57,35 +57,38 @@ void free_kernels(float **kernel, float kernel_dim) {
 }
 
 /* allocates rows and displacements, accounting for necessary overlaps */
-void assign_rows(int nproc, int height, int width, int *counts, int *allc,
-                 int *displs) {
+void assign_rows(int nproc, int height, int width, double std_dev, int *counts,
+                 int *allc, int *displs) {
     int rows_per_proc = height / nproc;
     int remainder = height % nproc;
-    int rows, overlap, offset = 0;
+    int accumulator = 0;
 
     for (int i = 0; i < nproc; i++) {
-        rows = rows_per_proc + (i < remainder ? 1 : 0);
-        overlap = (nproc > 1 ? (i == 0 || i == nproc - 1 ? 1 : 2) : 0);
-        counts[i] = (rows + overlap) * width * RGB;
-        allc[i] = rows * width * RGB;
-        displs[i] = offset;
-        offset += counts[i];
+        // allocate base rows excluding overlaps
+        allc[i] = rows_per_proc + (i < remainder ? 1 : 0);
+
+        // allocate counts including overlaps
+        counts[i] = (allc[i] + (nproc == 1 ? 0 : (i == 0 || i == nproc - 1 ?
+            3 * std_dev: 6 * std_dev))) * width * RGB;
+
+        // allocate displacements
+        displs[i] = accumulator;
+
+        accumulator += counts[i];
     }
 }
 
-/* flattens a BMP image to a 1D vector to be read in increments of 3 (RGB) */
-/* allows for offsets to exclude overlaps */
-void flatten_image(BMP *bmp, UCHAR *flattened_img, int height, int width,
-                   int top_offset, int bottom_offset) {
+void flatten_image(BMP *bmp, UCHAR *flat_img, int start, int stop,
+                   int width, int accum_offset) {
     UCHAR r, g, b;
-    int accumulator = 0;
+    int accumulator = accum_offset;
 
-    for (int row = 0 + top_offset; row < height - bottom_offset; row++) {
+    for (int row = start; row < stop; row++) {
         for (int col = 0; col < width; col++) {
             BMP_GetPixelRGB(bmp, col, row, &r, &g, &b);
-            flattened_img[accumulator] = r;
-            flattened_img[accumulator + 1] = g;
-            flattened_img[accumulator + 2] = b;
+            flat_img[accumulator] = r;
+            flat_img[accumulator + 1] = g;
+            flat_img[accumulator + 2] = b;
             accumulator += 3;
         }
     }
